bc784f688f8fd66303869d357ebf6f74dc269705
rlugones <rodrigolugones@gmail.com>
2016-03-10 17:44:39 -0300

    Se siguieron agregando funciones: add_float_ref, add_int_ref y add_int_array

--- libtester.py
+++ libtester.py
@@ -19,14 +19,18 @@ print(mm.add_int(a,b))
 #int add_float_ref(float *a, float *b, float *c) {*c = *a + *b; return 0;}
 a=C.c_float(2)
 b=C.c_float(5)
-c=C.c_float()
-#mm.add_float_ref.argtypes = [C.c_float,C.c_float,C.c_float]
-#mm.add_float_ref.restype = C.c_float
-mm.add_float_ref(C.byref(a),C.byref(b),C.byref(c))
-print(c.value)
+out=C.c_float()
+mm.add_float_ref(C.byref(a),C.byref(b),C.byref(out))
+print(out.value)
 
 
 #int add_int_ref(int *a, int *b, int *c) {*c = *a + *b; return 0;}
+a=C.c_int(2)
+b=C.c_int(5)
+out=C.c_int()
+mm.add_int_ref(C.byref(a),C.byref(b),C.byref(out))
+print(out.value)
+
 #int add_int_array(int *a, int *b, int *c, int n) {
 #  int i;
 #  for (i = 0; i < n; i++) {
@@ -34,7 +38,14 @@ print(c.value)
 #  }
 #  return 0;
 #}
-#
+a=(C.c_int * 3) (1,2,3)
+b=(C.c_int * 3) (4,5,6)
+out=(C.c_int * 3) (0,0,0)
+n=3#n=C.c_int(3)
+mm.add_int_array(C.byref(a),C.byref(b),C.byref(out),C.c_int(n))
+print(out[0],out[1],out[2])
+print(out)
+
 #float dot_product(float *a, float *b, int n) {
 #  float res;
 #  int i;
